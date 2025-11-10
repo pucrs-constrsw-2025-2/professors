c4290160ae70899b33e09513a2ff16cf4901832d
Andrei <andreirech22@outlook.com>
2025-11-10 09:37:48 -0300

    feat: add prometheus

--- professors/main.py
+++ professors/main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 import asyncio
+from prometheus_fastapi_instrumentator import Instrumentator  # <-- ADICIONE ESTA IMPORTAÇÃO
 
 from professors.config import get_settings
-from professors.adapters.api.routes import professors, classes, graduations # Importa o módulo
+from professors.adapters.api.routes import professors, classes, graduations  # Importa o módulo
 from professors.adapters.database.database import Base, engine
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Iniciando serviço Professors...")
     print(f"Conectando ao DB: {get_settings().DATABASE_URL.split('@')[-1]}")
-    
+
     print("Verificando e criando tabelas (se não existirem)...")
     try:
         await asyncio.to_thread(Base.metadata.create_all, bind=engine)
         print("Tabelas prontas.")
     except Exception as e:
         print(f"Erro ao criar tabelas: {e}")
-    
+
     yield
     print("Encerrando serviço Professors...")
+
 
 app = FastAPI(
     title="Professors Microservice",
@@ -30,7 +32,8 @@ app = FastAPI(
 
 app.include_router(professors.router)
 app.include_router(classes.router)
-app.include_router(graduations.router) 
+app.include_router(graduations.router)
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/health", tags=["Health Check"], summary="Verifica a saúde da API")
 def health_check():
